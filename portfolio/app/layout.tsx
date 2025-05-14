9c8e4c0d0b99fb8fb53d951dfdacaa1d1b8277cf
Natalia Jagiela <natalia.jagiela@stud-provadis-hochschule.de>
2025-05-14 13:00:52 +0000

    anfang

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -2,23 +2,26 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import Navbar from '../components/Navbar';
+import { EB_Garamond } from "next/font/google";
+
+const eb = EB_Garamond({
+  subsets: ["latin"],
+  weight: ["400", "400"], // oder was du brauchst
+  variable: "--font-eb", // CSS Variable setzen
+});
+
 
 export const metadata: Metadata = {
   title: 'My Portfolio',
   description: 'Created with Next.js and Tailwind',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="de">
-      <body>
+    <html lang="en" className={eb.variable}>
+      <body className=''>
         <Navbar />
-        <main className="min-h-screen">{children}</main>
-      </body>
+        {children}</body>
     </html>
   );
 }
